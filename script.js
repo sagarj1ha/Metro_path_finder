9f4c1f5c8ac898baae5288caba247617d4b8450f
sagarj1ha <111685221+sagarj1ha@users.noreply.github.com>
2024-05-08 18:24:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,300 @@
+let blocks = document.getElementsByClassName("drawing-area")[0];
+let addEdge = false;
+let cnt = 0;
+let dist;
+
+
+const addEdges = () => {
+    if (cnt < 2) {
+      alert("Create atleast two nodes to add an edge");
+      return;
+    }
+  
+    addEdge = true;
+    document.getElementById("add-edge-enable").disabled = true;
+    document.getElementsByClassName("run-btn")[0].disabled = false;
+    
+    dist = new Array(cnt + 1)
+      .fill(Infinity)
+      .map(() => new Array(cnt + 1).fill(Infinity));
+  };
+
+
+  let arr = [];
+const drawLine = (x1, y1, x2, y2, ar) => {
+    // prevent multiple edges for same couple of nodes
+    if (dist[Number(ar[0])][Number(ar[1])] !== Infinity) {
+      document.getElementById(arr[0]).style.backgroundColor = "#333";
+      document.getElementById(arr[1]).style.backgroundColor = "#333";
+      return;
+    }
+  
+    console.log(ar);
+    // Length of line
+    const len = Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+    const slope = x2 - x1 ? (y2 - y1) / (x2 - x1) : y2 > y1 ? 90 : -90;
+  
+    // Adding length to distance array
+    dist[Number(ar[0])][Number(ar[1])] = Math.round(len / 10);
+    dist[Number(ar[1])][Number(ar[0])] = Math.round(len / 10);
+  
+    // Drawing line
+    const line = document.createElement("div");
+    line.id =
+      Number(ar[0]) < Number(ar[1])
+        ? `line-${ar[0]}-${ar[1]}`
+        : `line-${ar[1]}-${ar[0]}`;
+    line.classList.add("line");
+    line.style.width = `${len}px`;
+    line.style.left = `${x1}px`;
+    line.style.top = `${y1}px`;
+  
+    // Edge weight
+    let p = document.createElement("p");
+    p.classList.add("edge-weight");
+    p.innerText = Math.round(len / 10);
+    p.contentEditable = "true";
+    p.inputMode = "numeric";
+    p.addEventListener("blur", (e) => {
+      if (isNaN(Number(e.target.innerText))) {
+        alert("Enter valid edge weight");
+        return;
+      }
+      n1 = Number(p.closest(".line").id.split("-")[1]);
+      n2 = Number(p.closest(".line").id.split("-")[2]);
+      // console.log(p.closest('.line'), e.target.innerText, n1, n2);
+      dist[n1][n2] = Number(e.target.innerText);
+      dist[n2][n1] = Number(e.target.innerText);
+    });
+    line.style.transform = `rotate(${
+      x1 > x2 ? Math.PI + Math.atan(slope) : Math.atan(slope)
+    }rad)`;
+  
+    p.style.transform = `rotate(${
+      x1 > x2 ? (Math.PI + Math.atan(slope)) * -1 : Math.atan(slope) * -1
+    }rad)`;
+  
+    line.append(p);
+    blocks.appendChild(line);
+    document.getElementById(arr[0]).style.backgroundColor = "#333";
+    document.getElementById(arr[1]).style.backgroundColor = "#333";
+  };
+  
+
+
+
+
+const drawUsingId = (ar) => {
+    if (ar[0] === ar[1]) {
+      document.getElementById(arr[0]).style.backgroundColor = "#333";
+      arr = [];
+      return;
+    }
+    x1 = Number(document.getElementById(ar[0]).style.left.slice(0, -2));
+    y1 = Number(document.getElementById(ar[0]).style.top.slice(0, -2));
+    x2 = Number(document.getElementById(ar[1]).style.left.slice(0, -2));
+    y2 = Number(document.getElementById(ar[1]).style.top.slice(0, -2));
+    drawLine(x1, y1, x2, y2, ar);
+  };
+
+
+
+
+
+
+
+
+  
+
+
+
+const appendBlock = (x, y) => {
+    document.querySelector(".reset-btn").disabled = false;
+    document.querySelector(".click-instruction").style.display = "none";
+    
+    const block = document.createElement("div");
+    block.classList.add("block");
+    block.style.top = `${y}px`;
+    block.style.left = `${x}px`;
+    block.style.transform = `translate(-50%,-50%)`;
+    block.id = cnt;
+    console.log("hey");
+  
+    block.innerText = cnt++;
+  
+  
+    block.addEventListener("click", (e) => {
+
+      e.stopPropagation() || (window.event.cancelBubble = "true");
+  
+      
+      if (!addEdge) return;
+  
+      block.style.backgroundColor = "coral";
+      arr.push(block.id);
+  
+      // When two elements are push, draw a edge and empty the array
+      if (arr.length === 2) {
+        drawUsingId(arr);
+        arr = [];
+      }
+    });
+    blocks.appendChild(block);
+  };
+
+
+
+blocks.addEventListener("click", (e) => {
+    if (addEdge) return;
+    if (cnt > 12) {
+      alert("cannot add more than 12 vertices");
+      return;
+    }
+    console.log(e.x, e.y);
+    appendBlock(e.x, e.y);
+  });
+
+
+
+  const findShortestPath = (el) => {
+    let visited = [];
+    let unvisited = [];
+    clearScreen();
+  
+    let source = Number(el.previousElementSibling.value);
+    if (source >= cnt || isNaN(source)) {
+      alert("Invalid source");
+      return;
+    }
+    document.getElementById(source).style.backgroundColor = "grey";
+   
+    let parent = [];
+    parent[source] = -1;
+    visited = [];
+    for (i = 0; i < cnt; i++) unvisited.push(i);
+  
+   
+    let cost = [];
+    for (i = 0; i < cnt; i++) {
+      i === source
+        ? null
+        : dist[source][i]
+        ? (cost[i] = dist[source][i])
+        : (cost[i] = Infinity);
+    }
+    cost[source] = 0;
+  
+    let minCost = [];
+    minCost[source] = 0;
+
+ 
+  
+
+    while (unvisited.length) {
+      let mini = cost.indexOf(Math.min(...cost));
+     
+    
+      visited.push(mini);
+      unvisited.splice(unvisited.indexOf(mini), 1);
+  
+      for (j of unvisited) {
+        if (j === mini) continue;
+        
+        if (cost[j] > dist[mini][j] + cost[mini]) {
+          minCost[j] = dist[mini][j] + cost[mini];
+          cost[j] = dist[mini][j] + cost[mini];
+          parent[j] = mini;
+        } else {
+          minCost[j] = cost[j];
+          
+        }
+      }
+      cost[mini] = Infinity;
+    }
+    console.log("Minimum Cost", minCost);
+    for (i = 0; i < cnt; i++)
+      parent[i] === undefined ? (parent[i] = source) : null;
+ 
+    indicatePath(parent, source,minCost);
+  };
+
+  const printPath = async (parent, j, el_p) => {
+    if (parent[j] === -1) return;
+    await printPath(parent, parent[j], el_p);
+    el_p.innerText = el_p.innerText + " " + j;
+  
+    document.getElementsByClassName("path")[0].style.padding = "1rem";
+    document.getElementsByClassName("path")[0].appendChild(el_p);
+  
+   
+  
+    if (j < parent[j]) {
+      let tmp = document.getElementById(`line-${j}-${parent[j]}`);
+      await colorEdge(tmp);
+    } else {
+      let tmp = document.getElementById(`line-${parent[j]}-${j}`);
+      await colorEdge(tmp);
+    }
+  };
+  
+  const colorEdge = async (el) => {
+    if (el.style.backgroundColor !== "aqua") {
+      await wait(1000);
+      el.style.backgroundColor = "aqua";
+    
+      el.style.height = "8px";
+    }
+  };
+
+
+  const wait = async (t) => {
+    let pr = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("done!");
+      }, t);
+    });
+    res = await pr;
+  };
+  const indicatePath = async (parentArr, src, minCost) => {
+    document.getElementsByClassName("path")[0].innerHTML = "";
+    console.log(minCost);
+    for (i = 0; i < cnt; i++) {
+           if(minCost[i]!==Infinity){
+        let p = document.createElement("p");
+        p.innerText = "Cost =>" +`${minCost[i]} `+"||  "+" Station " + i + " --> " + src;
+        await printPath(parentArr, i, p);
+    }
+      
+    }
+  };
+
+
+
+
+  
+
+
+const clearScreen = () => {
+    document.getElementsByClassName("path")[0].innerHTML = "";
+    let lines = document.getElementsByClassName("line");
+    for (line of lines) {
+      line.style.backgroundColor = "#EEE";
+      line.style.height = "5px";
+    }
+  };
+
+  const resetDrawingArea = () => {
+    blocks.innerHTML = "";
+
+    const p = document.createElement("p");
+    p.classList.add("click-instruction");
+    p.innerHTML = "Click to create STATION";
+  
+    blocks.appendChild(p);
+    document.getElementById("add-edge-enable").disabled = false;
+    document.querySelector(".reset-btn").disabled = true;
+    document.getElementsByClassName("path")[0].innerHTML = "";
+  
+    cnt = 0;
+    dist = [];
+    addEdge = false;}
